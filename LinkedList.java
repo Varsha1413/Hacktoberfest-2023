2af704e8f73040e4c5cc485ad4471ef2b86ee9c1
Varsha1413 <91167781+Varsha1413@users.noreply.github.com>
2023-10-29 00:07:51 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,55 @@
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void print() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println("List size: " + list.size()); // Output: List size: 3
+        list.print(); // Output: 1 2 3
+    }
+}
